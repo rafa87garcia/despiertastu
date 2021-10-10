5b5c325b6d060c4af6de1e2e3de57615edd71315
Rafa Garcia Rea <rafagarciarea@gmail.com>
2021-10-10 13:24:47 +0200

    Añadir tema despiestastu

---
+++ docroot/themes/custom/despiertastu_varbase/js/scripts.js
@@ -0,0 +1,12 @@
+/**
+ * @file
+ * Behaviors for the despiertastu_varbase theme.
+ */
+
+(function($, _, Drupal) {
+  Drupal.behaviors.despiertastu_varbase = {
+    attach: function() {
+      // despiertastu_varbase JavaScript behaviors goes here.
+    }
+  };
+})(window.jQuery, window._, window.Drupal);
